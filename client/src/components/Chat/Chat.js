4d4ddd95637a4834774302cb01ee5b86c254f699
Diego Ferrer <diegofs46@users.noreply.github.com>
2020-05-08 15:07:25 -0400

    Deployment

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -21,7 +21,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://faschat.herokuapp.com/';
 
   
 
